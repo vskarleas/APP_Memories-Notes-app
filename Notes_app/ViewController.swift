e15d00aa770bda5cd26534467a203408538e307a
vskarleas <vskarleas@icloud.com>
2021-03-30 22:12:12 +0300

    Initial Commit

---
+++ Notes_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes_app
+//
+//  Created by Vasilis Skarleas on 30/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
